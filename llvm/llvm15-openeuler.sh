c7278b0d8685ae323976e60b8bbb9647655256ec
LiaoChunyu <chunyu@iscas.ac.cn>
2023-06-21 09:50:21 +0800

    ruyibuild for llvm

---
+++ llvm/llvm15-openeuler.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -x
+
+yum install -y cmake ninja-build autoconf automake python3 libmpc-devel mpfr-devel gmp-devel gawk bison flex texinfo patchutils gcc gcc-c++ zlib-devel expat-devel make diffutils
+cd /home/src
+cmake -DLLVM_ENABLE_PROJECTS="clang" -DCMAKE_BUILD_TYPE="Release"  -G Ninja ./llvm
+ninja
