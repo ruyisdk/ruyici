7f3ed1664953a8966170786c07c01569c446be57
LiaoChunyu <chunyu@iscas.ac.cn>
2023-06-21 12:56:10 +0800

    update build

--- llvm/llvm15-openeuler.sh
+++ llvm/llvm15-openeuler.sh
@@ -3,6 +3,6 @@
 set -x
 
 yum install -y cmake ninja-build autoconf automake python3 libmpc-devel mpfr-devel gmp-devel gawk bison flex texinfo patchutils gcc gcc-c++ zlib-devel expat-devel make diffutils
-cd /home/src
-cmake -DLLVM_ENABLE_PROJECTS="clang" -DCMAKE_BUILD_TYPE="Release"  -G Ninja ./llvm
+cd /home/build/$1
+cmake -DLLVM_ENABLE_PROJECTS="clang" -DCMAKE_BUILD_TYPE="Release"  -G Ninja ../../src/llvm
 ninja
