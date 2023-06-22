ebfb804228b110a173cca34d4276b2c33363af92
Weiwei Li <liweiwei@iscas.ac.cn>
2023-06-22 22:08:22 +0800

    enable slirp for qemu-system

--- qemu/qemu_oe.sh
+++ qemu/qemu_oe.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # install dependency
-yum install gcc git make glib2-devel.x86_64 ninja-build libcap-ng-devel.x86_64 libattr-devel.x86_64
+yum install gcc git make glib2-devel.x86_64 ninja-build libcap-ng-devel.x86_64 libattr-devel.x86_64 libslirp-devel.x86_64
 
 cd /home/src
 mkdir build
 cd build
-../configure --target-list=riscv64-softmmu,riscv64-linux-user,riscv32-softmmu,riscv32-linux-user --prefix=/home/build/$1 --disable-werror --enable-virtfs
+../configure --target-list=riscv64-softmmu,riscv64-linux-user,riscv32-softmmu,riscv32-linux-user --prefix=/home/build/$1 --disable-werror --enable-virtfs --enable-slirp
 make -j $(nproc)
 make install
--- qemu/qemu_ubuntu2204.sh
+++ qemu/qemu_ubuntu2204.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # install dependency
-apt install gcc git make ninja-build python3 python3-pip libglib2.0-dev libpixman-1-dev
+apt install gcc git make ninja-build python3 python3-pip libglib2.0-dev libpixman-1-dev libslirp-dev
 # apt build-dep qemu
 
 cd /home/src
 mkdir build
 cd build
-../configure --target-list=riscv64-softmmu,riscv64-linux-user,riscv32-softmmu,riscv32-linux-user --prefix=/home/build/$1 --disable-werror --enable-virtfs
+../configure --target-list=riscv64-softmmu,riscv64-linux-user,riscv32-softmmu,riscv32-linux-user --prefix=/home/build/$1 --disable-werror --enable-virtfs --enable-slirp
 make -j $(nproc)
 make install
