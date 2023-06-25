2d9e3119644baf66102d52a6f6f96ee9abb86554
Jiawei <jiawei@iscas.ac.cn>
2023-06-25 22:53:24 +0800

    Add GNU toolchain build ci.

---
+++ gcc/ubuntu/22.04/rv64ilp32/gcc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+
+apt-get update
+apt-get install -y autoconf automake autotools-dev curl python3 python3-pip libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev ninja-build git cmake libglib2.0-dev
+cd /home/src
+./configure --prefix=/home/build/$1 --with-arch=rv64imac --with-abi=ilp32
+make linux -j $(nproc)
