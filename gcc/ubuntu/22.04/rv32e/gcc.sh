0c2e2de6f2d100605ff39323f86a939fb36eb4dc
Jiawei <jiawei@iscas.ac.cn>
2023-06-27 00:08:11 +0800

    Update toolchain ci files.

--- gcc/ubuntu/22.04/rv32e/gcc.sh
+++ gcc/ubuntu/22.04/rv32e/gcc.sh
@@ -5,5 +5,5 @@ set -x
 apt-get update
 apt-get install -y autoconf automake autotools-dev curl python3 python3-pip libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev ninja-build git cmake libglib2.0-dev
 cd /home/src
-./configure --prefix=/home/build/$1 --with-arch=rv64gcv0p7
-make linux -j $(nproc)
+./configure --prefix=/home/build/$1 --with-arch=rv32ec_zcb_zmmul --with-abi=ilp32e
+make -j $(nproc)
