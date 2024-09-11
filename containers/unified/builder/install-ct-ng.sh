799930d45cf7802ffb397276ae1e887c98df722a
WANG Xuerui <wangxuerui@iscas.ac.cn>
2024-09-11 17:09:58 +0800

    containers: bump unified builder image, use git ct-ng

--- containers/unified/builder/install-ct-ng.sh
+++ containers/unified/builder/install-ct-ng.sh
@@ -2,27 +2,73 @@
 
 set -e
 
-CT_NG_FILENAME="crosstool-ng-${CT_NG_VERSION}.tar.xz"
-CT_NG_PATCH_DIR=/image/ct-ng-patches
+: "${CT_NG_GIT_BUILD:=false}"
 
-cd /tmp
-wget "${CT_NG_DOWNLOAD_URL_BASE}/${CT_NG_FILENAME}"
-# TODO: signature verification
-
-mkdir ct-ng-tmp
-pushd ct-ng-tmp
-tar -xf "/tmp/${CT_NG_FILENAME}" --strip-components=1
-
-if [[ -d ${CT_NG_PATCH_DIR} ]]; then
-    for p in "${CT_NG_PATCH_DIR}"/*.patch; do
-        echo "Applying $p..."
-        patch -Np1 < "$p"
-    done
+if "$CT_NG_GIT_BUILD"; then
+	: "${CT_NG_GIT_REPO:=https://github.com/crosstool-ng/crosstool-ng.git}"
+	: "${CT_NG_GIT_BRANCH:=master}"
+else
+	: "${CT_NG_DOWNLOAD_URL_BASE:?CT_NG_DOWNLOAD_URL_BASE must be set for non-git builds}"
+	: "${CT_NG_VERSION:?CT_NG_VERSION must be set for non-git builds}"
+	CT_NG_FILENAME="crosstool-ng-${CT_NG_VERSION}.tar.xz"
 fi
 
-./configure --prefix=/usr/local
-make -j4
-make install
-popd
+CT_NG_PATCH_DIR=/image/ct-ng-patches
+CT_NG_CHECKOUT=/tmp/ct-ng-tmp
 
-rm -rf ct-ng-tmp
+fetch() {
+	if "$CT_NG_GIT_BUILD"; then
+		local git_clone_options=(
+			--depth 1
+			--recurse-submodules
+			--shallow-submodules
+			-b "$CT_NG_GIT_BRANCH"
+		)
+		git clone "${git_clone_options[@]}" "$CT_NG_GIT_REPO" "$CT_NG_CHECKOUT"
+	else
+		wget "${CT_NG_DOWNLOAD_URL_BASE}/${CT_NG_FILENAME}"
+		# TODO: signature verification
+		mkdir -p "$CT_NG_CHECKOUT"
+		pushd "$CT_NG_CHECKOUT" > /dev/null
+		tar -xf "/tmp/$CT_NG_FILENAME" --strip-components=1
+		popd > /dev/null
+	fi
+}
+
+prepare() {
+	pushd "$CT_NG_CHECKOUT" > /dev/null
+
+	if [[ -d $CT_NG_PATCH_DIR ]]; then
+		for p in "$CT_NG_PATCH_DIR"/*.patch; do
+			echo "Applying $p..."
+			patch -Np1 < "$p"
+		done
+	fi
+
+	"$CT_NG_GIT_BUILD" && ./bootstrap
+
+	popd > /dev/null
+}
+
+build_and_install() {
+	pushd "$CT_NG_CHECKOUT" > /dev/null
+	./configure --prefix=/usr/local
+	make -j4
+	make install
+	popd > /dev/null
+}
+
+clean() {
+	rm -rf "$CT_NG_CHECKOUT"
+	[[ -n "$CT_NG_FILENAME" ]] && rm "/tmp/$CT_NG_FILENAME" || true
+}
+
+main() {
+	cd /tmp
+	fetch
+	prepare
+	build_and_install
+	clean
+}
+
+main
