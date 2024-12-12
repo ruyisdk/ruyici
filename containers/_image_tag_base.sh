5d76614628bf49bb659f9471f4924c71b1e547ef
WANG Xuerui <wangxuerui@iscas.ac.cn>
2024-12-12 23:12:09 +0800

    refactor: fix some shellcheck lints

--- containers/_image_tag_base.sh
+++ containers/_image_tag_base.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # this file is meant to be sourced
 
 image_tag_pkgbuilder_unified() {
@@ -8,7 +9,7 @@ image_tag_pkgbuilder_unified() {
             echo "ghcr.io/ruyisdk/ruyi-ci-pkgbuilder-unified:20240911"
             ;;
         *)
-            echo "error: image_tag_pkgbuilder_unified: unsupported host arch $arch" >&2
+            echo "error: image_tag_pkgbuilder_unified: unsupported host arch $host_arch" >&2
             return 1
             ;;
     esac
