ea9113c59a09a3bf9bb571a1bcd6ce5ef3529f08
WANG Xuerui <wangxuerui@iscas.ac.cn>
2025-05-24 12:47:42 +0800

    containers/rust-musl: add
    Signed-off-by: WANG Xuerui <wangxuerui@iscas.ac.cn>

--- containers/_image_tag_base.sh
+++ containers/_image_tag_base.sh
@@ -20,6 +20,19 @@ image_tag_pkgbuilder() {
         esac
         ;;
 
+    rust-musl)
+        tag="ghcr.io/ruyisdk/ruyi-ci-pkgbuilder-rust-musl"
+        case "$host_arch" in
+        amd64)
+            echo "$tag:20250523"
+            ;;
+        *)
+            echo "error: image_tag_pkgbuilder: unsupported host arch $host_arch" >&2
+            return 1
+            ;;
+        esac
+        ;;
+
     *)
         echo "error: image_tag_pkgbuilder: unsupported image kind $kind" >&2
         ;;
