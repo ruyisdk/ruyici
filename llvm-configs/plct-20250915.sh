9ddd499df381cb5a55e44223a52c24a8cb5e78ac
WANG Xuerui <wangxuerui@iscas.ac.cn>
2025-09-15 15:44:37 +0800

    llvm: add 20250915 build, more cross build work
    Signed-off-by: WANG Xuerui <wangxuerui@iscas.ac.cn>

---
+++ llvm-configs/plct-20250915.sh
@@ -0,0 +1,5 @@
+P="llvm-plct-21.1.0"
+PV="21.1.0"
+RUYI_DATESTAMP=20250915
+SRC_URI="https://mirror.iscas.ac.cn/ruyisdk/dist/ruyisdk-llvm-project-3623fe661ae35c6c80ac221f14d85be76aa870f.src.tar.zst"
+TAR_STRIP_COMPONENTS=2
